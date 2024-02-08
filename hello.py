296db262be6d2b394b80436943be5af9793168ef
Emmanuel.62 <hubertone26@gmail.com>
2024-02-08 13:59:07 +0100

    Troisieme commit

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ print("Bienvenue dans le monde de Python")
 
 nom = input("Quel est votre nom? ")
 prenom = input("Quel est votre prénom? ")
-age = input("Quel est votre âge? ") 
+age = input("Quel est votre âge? ")
 
 print("Bonjour", prenom, nom, age, "ans")
+
+annee = 2024 - int(age)
+
+print("En 2024, vous étez de l'année ", annee, "ans")  
