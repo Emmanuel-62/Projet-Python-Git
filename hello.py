652f6e58b9e18cbcea45942f96e34d305bb4636e
Emmanuel.62 <hubertone26@gmail.com>
2024-02-08 13:49:40 +0100

    Second commit

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ print("Bienvenue dans le monde de Python")
 
 nom = input("Quel est votre nom? ")
 prenom = input("Quel est votre prénom? ")
+age = input("Quel est votre âge? ") 
 
-print("Bonjour", prenom, nom)
+print("Bonjour", prenom, nom, age, "ans")
